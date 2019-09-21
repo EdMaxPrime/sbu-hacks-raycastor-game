aef07ab9eb72a30dc3be62952e1b403e62fd000a
Max Zlotskiy <max.zlotskiy@gmail.com>
2019-09-20 23:17:12 -0400

    fixed indentation in raycastor

--- RayCastor.js
+++ RayCastor.js
@@ -1,28 +1,28 @@
 class RayCastor {
-  constructor(c, w) {
+	constructor(c, w) {
 		this.world = w;
-    this.camera = c;
+		this.camera = c;
 		this.renderDistance = 20; //how many squares away you can see
-    this.stripes = [];
+		this.stripes = [];
 		this.floor = [];
 		this.max = 0;
-  }
+	}
 	beginCasting() {
 		// //green lines
 		//fill(0);
 		//rect(width/2, height/2, 200, -200);
-    this.camera.reset();
-    this.stripes = []; //an array of length "camera.resolution" containing Image objects
+		this.camera.reset();
+		this.stripes = []; //an array of length "camera.resolution" containing Image objects
 		var stackOfHitObjects = [];
 		var pointsOnFloor = [];
-    var rayNumber = 0;
-    var solid = null;
-    while (this.camera.hasNextRay()) {
-      var r = this.camera.nextRay();
+		var rayNumber = 0;
+		var solid = null;
+		while (this.camera.hasNextRay()) {
+			var r = this.camera.nextRay();
 			stackOfHitObjects = [];
-      for (var i = 0; i < this.renderDistance; i ++) {
-        solid = this.world.whatsThere(r.getMapX(), r.getMapY());
-				
+			for (var i = 0; i < this.renderDistance; i ++) {
+				solid = this.world.whatsThere(r.getMapX(), r.getMapY());
+
 				// //green lines
 				// if(solid != null) {
 				// 	stroke(128, 255, 128);
@@ -37,22 +37,22 @@ class RayCastor {
 				where -= floor(where); //we only need the decimal portion of this variable
 				let wallDist = r.perpWallDist();
 				pointsOnFloor.push([this.world.whatsThere(r.getMapX(), r.getMapY(), 0), r.getMapX(), r.getMapY(), wallDist]);
-        if (solid == null) {
-          r.grow(); //nothing there, keep going
-        } else if (solid.opacity < 1) {
+				if (solid == null) {
+					r.grow(); //nothing there, keep going
+				} else if (solid.opacity < 1) {
 					stackOfHitObjects.push([solid, r.sideHit, where, wallDist]);
-          r.grow();
-        } else {
+					r.grow();
+				} else {
 					//if(wallDist > this.max) this.max = wallDist;
 					stackOfHitObjects.push([solid, r.sideHit, where, wallDist]);
-          break; //we hit something opaque
-        }
-      }
+		  			break; //we hit something opaque
+	  			}
+			}
 			this.floor[rayNumber] = pointsOnFloor;
 			this.stripes[rayNumber] = stackOfHitObjects;
-      rayNumber++;
-    }
-  }
+			rayNumber++;
+		}
+	}
 	floorCast(x, y, numHorizontalStripes) {
 		/*var furthestWall = floor[i][floor[i].length - 1].distance;
 			for(var y = (height + height / furthestWall)/2; y < height; y += w) {
@@ -73,28 +73,28 @@ class RayCastor {
 	}
 	/* Returns the solid/entity closest to the camera using the center ray */
 	lookingAt() {
-    var r = this.camera.centerRay();
-    var solid = null;
-    for(var i = 0; i < this.renderDistance; i++) {
-      solid = this.world.whatsThere(r.getMapX(), r.getMapY());
-      if(solid == null) r.grow(); //nothing there
-      else break; //hit something
-    }
-    return s;
-  }
+		var r = this.camera.centerRay();
+		var solid = null;
+		for(var i = 0; i < this.renderDistance; i++) {
+			solid = this.world.whatsThere(r.getMapX(), r.getMapY());
+			if(solid == null) r.grow(); //nothing there
+			else break; //hit something
+		}
+		return s;
+	}
 	/* Returns the array of stripes to be drawn drawn */
 	getTerrainBuffer() {
-    return this.stripes;
-  }
+		return this.stripes;
+	}
 	/* Returns an array of floor textures to be drawn under each stripe, as well as the distance they are away from the player [texture, x, y, distance] */
 	getFloorBuffer() {
 		return this.floor;
 	}
 	/* Sets the maximum distance rays will be projected. */
 	setRenderDistance(d) {
-    if(d > 1 && d < 100) this.renderDistance = d;
-  }
+		if(d > 1 && d < 100) this.renderDistance = d;
+	}
 	getRenderDistance() {
-    return this.renderDistance;
-  }
+		return this.renderDistance;
+	}
 }
